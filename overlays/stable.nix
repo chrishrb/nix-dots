aa8db5be31a14a9e997a95300adc224fb84bc227
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2024-09-10 17:48:51 +0200

    SAM cli back to unstable

--- overlays/stable.nix
+++ overlays/stable.nix
@@ -1,6 +1,5 @@
 # INFO: these packages are buggy and don‘t work in unstable
 inputs: _final: prev: {
-  aws-sam-cli = inputs.nixpkgs-stable.legacyPackages.${prev.system}.aws-sam-cli;
   colima = inputs.nixpkgs-stable.legacyPackages.${prev.system}.colima;
   eslint = inputs.nixpkgs-stable.legacyPackages.${prev.system}.nodePackages.eslint;
 }
