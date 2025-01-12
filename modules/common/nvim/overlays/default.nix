38b0b3db1200f800a5b68b05e5119f650ffe73b7
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2025-01-12 11:41:00 +0100

    fix codecompanion

--- modules/common/nvim/overlays/default.nix
+++ modules/common/nvim/overlays/default.nix
@@ -1,10 +1,12 @@
 inputs: _final: prev: {
   vimPlugins = prev.vimPlugins.extend (final': prev': {
-    # codecompanion-nvim = prev.vimUtils.buildVimPlugin {
-    #   pname = "codecompanion.nvim";
-    #   version = "2024-09-16";
-    #   src = inputs.codecompanion-nvim;
-    # };
+    codecompanion-nvim = prev.vimUtils.buildVimPlugin {
+      pname = "codecompanion.nvim";
+      version = "2024-09-16";
+      src = inputs.codecompanion-nvim;
+      dependencies = [ prev.vimPlugins.plenary-nvim ];
+      nvimRequireCheck = "codecompanion";
+    };
     nvim-tmux-navigation = prev.vimUtils.buildVimPlugin {
       pname = "nvim-tmux-navigation";
       version = "2024-03-26";
