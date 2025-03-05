e3e8b04b5cb24f18c500ad446eecad3afb9140bb
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2025-03-05 10:39:38 +0100

    fix autoformatter issue

--- modules/common/nvim/lua/chrishrb/lsp/null-ls.lua
+++ modules/common/nvim/lua/chrishrb/lsp/null-ls.lua
@@ -15,21 +15,28 @@ null_ls.setup({
 	sources = {
 		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 		formatting.stylua,
-    diagnostics.pylint,
-    diagnostics.mypy,
-    formatting.isort,
-    formatting.nixfmt
+		diagnostics.pylint,
+		diagnostics.mypy,
+		diagnostics.eslint,
+		formatting.isort,
+		formatting.nixfmt,
 	},
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end
-  end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						bufnr = bufnr,
+						filter = function(c)
+							return c.name == "null-ls"
+						end,
+						async = false,
+					})
+				end,
+			})
+		end
+	end,
 })
