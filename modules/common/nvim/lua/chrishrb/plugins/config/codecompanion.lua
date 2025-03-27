55acf9d62c498ba6d9695bc6ba37e381638eae07
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2025-03-27 11:22:50 +0100

    improve codecompanion shortcuts

--- modules/common/nvim/lua/chrishrb/plugins/config/codecompanion.lua
+++ modules/common/nvim/lua/chrishrb/plugins/config/codecompanion.lua
@@ -1,52 +1,87 @@
 local adapter = nixCats("aiAdapter")
 if not adapter then
-  return
+	return
 end
 
+local codecompanion = require("codecompanion")
+local context_utils = require("codecompanion.utils.context")
+local api = vim.api
+local prefix = "🤖 ~ "
+
 require("dressing").setup({
-  input = {
-    enabled = false
-  },
-  select = {
-    enabled = true
-  }
+	input = {
+		enabled = false,
+	},
+	select = {
+		enabled = true,
+	},
 })
 
-require("codecompanion").setup({
-  display = {
-    action_palette = {
-      prompt = "🤖 ~ "
-    }
-  },
-  strategies = {
-    chat = {
-      adapter = adapter,
-    },
-    inline = {
-      adapter = adapter,
-    },
-    agent = {
-      adapter = adapter,
-    },
-  },
+codecompanion.setup({
+	display = {
+		action_palette = {
+			prompt = prefix,
+		},
+		chat = {
+			intro_message = "",
+			start_in_insert_mode = false,
+		},
+	},
+	strategies = {
+		chat = {
+			adapter = adapter,
+		},
+		inline = {
+			adapter = adapter,
+		},
+		agent = {
+			adapter = adapter,
+		},
+	},
 })
 
 -- setup which-key mappings
 local which_key = require("which-key")
 which_key.add({
-  {
-    { "<leader>c", group = "CodeCompanion (" .. adapter .. ")", nowait = true, remap = false },
-    { "<leader>cc", "<cmd>CodeCompanion<CR>", desc = "Quick chat", nowait = true, remap = false },
-    { "<leader>ca", "<cmd>CodeCompanionActions<CR>", desc = "Actions", nowait = true, remap = false },
-    { "<leader>co", "<cmd>CodeCompanionChat<CR>", desc = "New Chat", nowait = true, remap = false },
-    { "<leader>ct", "<cmd>CodeCompanionChat Toggle<CR>", desc = "Toggle chat", nowait = true, remap = false },
-    {
-      mode = { "v" },
-      { "<leader>c", group = "CodeCompanion (" .. adapter .. ")", nowait = true, remap = false },
-      { "<leader>cc", ":CodeCompanion<CR>", desc = "Quick chat", nowait = true, remap = false },
-      { "<leader>ca", ":CodeCompanionActions<CR>", desc = "Actions", nowait = true, remap = false },
-    },
-  }
+	{
+		{ "<leader>c", group = "CodeCompanion (" .. adapter .. ")", nowait = true, remap = false },
+		{
+			"<leader>cc",
+			function()
+				local input = vim.fn.input(prefix)
+				if input ~= "" then
+					codecompanion.chat({ fargs = { "chat" }, args = input, range = 0 })
+				end
+			end,
+			desc = "Quick chat",
+			nowait = true,
+			remap = false,
+		},
+		{ "<leader>ci", "<cmd>CodeCompanion<CR>", desc = "Inline chat", nowait = true, remap = false },
+		{ "<leader>ca", "<cmd>CodeCompanionActions<CR>", desc = "Actions", nowait = true, remap = false },
+		{ "<leader>co", "<cmd>CodeCompanionChat<CR>", desc = "New Chat", nowait = true, remap = false },
+		{ "<leader>ct", "<cmd>CodeCompanionChat Toggle<CR>", desc = "Toggle chat", nowait = true, remap = false },
+		{
+			mode = { "v" },
+			{ "<leader>c", group = "CodeCompanion (" .. adapter .. ")", nowait = true, remap = false },
+			{ "<leader>cc", ":CodeCompanion<CR>", desc = "Quick chat", nowait = true, remap = false },
+			{ "<leader>ca", ":CodeCompanionActions<CR>", desc = "Actions", nowait = true, remap = false },
+			{
+				"<leader>co",
+				function()
+					local input = vim.fn.input(prefix)
+					if input ~= "" then
+						local context = context_utils.get(api.nvim_get_current_buf(), {})
+						local content = input .. "\n\nHere is the code from " .. context.filename .. ":\n\n"
+						codecompanion.chat({ fargs = { "chat" }, args = content, range = 1 })
+					end
+				end,
+				desc = "Chat with selection",
+				nowait = true,
+				remap = false,
+			},
+		},
+	},
 })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
