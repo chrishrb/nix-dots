e1179ad2cb2801519a38bfb13d36bce844f0ba00
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2026-04-07 13:52:07 +0200

    remove old stuff

--- modules/nixos/graphical/default.nix
+++ modules/nixos/graphical/default.nix
@@ -8,10 +8,6 @@
   ];
 
   config = lib.mkIf config.gui.enable {
-
-    # control brightness
-    programs.light.enable = true;
-
     services = {
       # Enable touchpad support
       libinput.enable = true;
